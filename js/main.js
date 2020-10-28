f9069acb712f19382656235e3a78c5f75186af87
Krendil766 <krendilek766@gmail.com>
2020-10-28 18:00:50 +0300

    add authorization_one

--- js/main.js
+++ js/main.js
@@ -8,3 +8,51 @@ close.addEventListener("click", toggleModal);
 function toggleModal() {
   modal.classList.toggle("is-open");
 }
+
+
+
+//day one
+const buttonAuth=document.querySelector('.button-auth');
+const modalAuth=document.querySelector('.modal-auth');
+const closeAuth=document.querySelector('.close-auth');
+const logInForm=document.querySelector('#logInForm');
+const loginInput=document.querySelector('#login');
+const userName=document.querySelector('.user-name');
+const buttonOut=document.querySelector('.button-out')
+
+let login="";
+
+
+function toggleModalAuth(){
+  modalAuth.classList.toggle('is-open')
+}
+
+function authorized(){
+ console.log('Авторизован');
+ buttonAuth.style.display ='none';
+ userName.style.display='inline';
+ buttonOut.style.display='block';
+};
+
+function noAuthorized(){
+  console.log('Не авторизован');
+  function logIn(event){
+    event.preventDefault();
+    login=loginInput.value;
+    toggleModalAuth();
+    console.log(login);
+    checkAuth();
+  }
+  buttonAuth.addEventListener('click', toggleModalAuth);
+  closeAuth.addEventListener('click',toggleModalAuth);
+  logInForm.addEventListener('submit',logIn)
+}
+
+function checkAuth(){
+  if (login){
+    authorized();
+  }else{
+    noAuthorized();
+  }
+};
+checkAuth();
