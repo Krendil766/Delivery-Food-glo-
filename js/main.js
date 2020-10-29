0a610b6f27ff668c4d610e25bd6f9188f979f500
Krendil766 <krendil766@gmail.com>
2020-10-29 22:13:30 +0300

    slider and valid password

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ const modalAuth = document.querySelector('.modal-auth');
 const closeAuth = document.querySelector('.close-auth');
 const logInForm = document.querySelector('#logInForm');
 const loginInput = document.querySelector('#login');
+const passwordInput = document.querySelector('#password');
 const userName = document.querySelector('.user-name');
 const buttonOut = document.querySelector('.button-out');
 const cardsRestaurants = document.querySelector('.cards-restaurants');
@@ -20,85 +21,100 @@ const logo = document.querySelector('.logo');
 const cardsMenu = document.querySelector('.cards-menu');
 
 let login = localStorage.getItem('foodDelivery');
+let password = localStorage.getItem('passPeople');
+
+function validName(str) {
+    const regName = /^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$/;
+    return regName.test(str);
+}
+
+function validPassword(str) {
+    const regPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/;
+    return regPassword.test(str);
+}
 
 function toggleModalAuth() {
-  modalAuth.classList.toggle('is-open');
-  loginInput.style.borderColor = '';
-  logInForm.reset();
-  if (modalAuth.classList.contains('is-open')) {
-    disableScroll();
-  } else {
-    enableScroll();
-  }
+    modalAuth.classList.toggle('is-open');
+    loginInput.style.borderColor = '';
+    logInForm.reset();
+    if (modalAuth.classList.contains('is-open')) {
+        disableScroll();
+    } else {
+        enableScroll();
+    }
 }
 
 function toggleModal() {
-  modal.classList.toggle("is-open");
+    modal.classList.toggle("is-open");
 }
 
 function authorized() {
-  function logOut() {
-    login = null;
-    localStorage.removeItem('foodDelivery')
-    buttonAuth.style.display = '';
-    userName.style.display = '';
-    buttonOut.style.display = '';
-    buttonOut.removeEventListener('click', logOut)
-    checkAuth();
-  }
-  userName.innerHTML = login;
-  buttonAuth.style.display = 'none';
-  userName.style.display = 'inline';
-  buttonOut.style.display = 'block';
-  buttonOut.addEventListener('click', logOut);
-  console.log('avtorizovan');
+    function logOut() {
+        login = null;
+        password = null;
+        localStorage.removeItem('foodDelivery');
+        localStorage.removeItem('passPeople')
+        buttonAuth.style.display = '';
+        userName.style.display = '';
+        buttonOut.style.display = '';
+        buttonOut.removeEventListener('click', logOut)
+        checkAuth();
+    }
+    userName.innerHTML = login;
+    buttonAuth.style.display = 'none';
+    userName.style.display = 'inline';
+    buttonOut.style.display = 'block';
+    buttonOut.addEventListener('click', logOut);
+    console.log('avtorizovan');
 };
 
 function noAuthorized() {
-  console.log('ne avtorizovan');
+    console.log('ne avtorizovan');
 
-  function logIn(event) {
-    event.preventDefault();
-    if (loginInput.value.trim()) {
-      login = loginInput.value;
-      toggleModalAuth();
-      localStorage.setItem('foodDelivery', login);
-      buttonAuth.removeEventListener('click', toggleModalAuth);
-      closeAuth.removeEventListener('click', toggleModalAuth);
-      logInForm.removeEventListener('submit', logIn);
-      logInForm.reset();
-      checkAuth();
+    function logIn(event) {
+        event.preventDefault();
+        if (validName(loginInput.value) && validPassword(passwordInput.value)) {
+            login = loginInput.value;
+            password = passwordInput.value;
+            toggleModalAuth();
+            localStorage.setItem('foodDelivery', login);
+            localStorage.setItem('passPeople', password);
+            buttonAuth.removeEventListener('click', toggleModalAuth);
+            closeAuth.removeEventListener('click', toggleModalAuth);
+            logInForm.removeEventListener('submit', logIn);
+            logInForm.reset();
+            checkAuth();
 
-    } else {
-      loginInput.style.borderColor = 'red';
-      loginInput.value = '';
+        } else {
+            loginInput.style.borderColor = 'red';
+            loginInput.value = '';
+        }
     }
-  }
 
 
-  cartButton.addEventListener("click", toggleModal);
-  close.addEventListener("click", toggleModal);
-  buttonAuth.addEventListener('click', toggleModalAuth);
-  closeAuth.addEventListener('click', toggleModalAuth);
-  logInForm.addEventListener('submit', logIn);
-  modalAuth.addEventListener('click', function (e) {
-    if (e.target.classList.contains('is-open')) {
-      toggleModalAuth();
-    }
-  })
+    cartButton.addEventListener("click", toggleModal);
+    close.addEventListener("click", toggleModal);
+    buttonAuth.addEventListener('click', toggleModalAuth);
+    closeAuth.addEventListener('click', toggleModalAuth);
+    logInForm.addEventListener('submit', logIn);
+    modalAuth.addEventListener('click', function(e) {
+        if (e.target.classList.contains('is-open')) {
+            toggleModalAuth();
+        }
+    })
 }
 
 function checkAuth() {
-  if (login) {
-    authorized();
-  } else {
-    noAuthorized();
-  }
+    if (login) {
+        authorized();
+    } else {
+        noAuthorized();
+    }
 };
 //day2
 
 function createCardsRestaurant() {
-  const card = `
+    const card = `
   <a class="card card-restaurant">
                         <img src="img/pizza-plus/preview.jpg" alt="image" class="card-image" />
                         <div class="card-text">
@@ -116,13 +132,13 @@ function createCardsRestaurant() {
                         </div>
                     </a>
   `;
-  cardsRestaurants.insertAdjacentHTML('afterbegin', card)
+    cardsRestaurants.insertAdjacentHTML('afterbegin', card)
 };
 
 function createCardGood() {
-  const card = document.createElement('div');
-  card.className = 'card';
-  card.insertAdjacentHTML('beforeend', `
+    const card = document.createElement('div');
+    card.className = 'card';
+    card.insertAdjacentHTML('beforeend', `
                          <img src="img/pizza-plus/pizza-classic.jpg" alt="image" class="card-image" />
                         <div class="card-text">
                             <div class="card-heading">
@@ -141,34 +157,34 @@ function createCardGood() {
                             </div>
                         </div>
    `);
-  cardsMenu.insertAdjacentElement('afterbegin', card);
+    cardsMenu.insertAdjacentElement('afterbegin', card);
 }
 
 function openGoods(e) {
-  const target = e.target;
-  const restaurant = target.closest('.card-restaurant');
-  toggleModalAuth();
-  if (login) {
-    if (restaurant) {
-      console.log('++++');
-      containerPromo.classList.add('hide');
-      restaurants.classList.add('hide');
-      menu.classList.remove('hide');
-      cardsMenu.textContent = '';
-      createCardGood();
-      createCardGood();
-      createCardGood();
-      createCardGood();
-      toggleModalAuth();
+    const target = e.target;
+    const restaurant = target.closest('.card-restaurant');
+    toggleModalAuth();
+    if (login) {
+        if (restaurant) {
+            console.log('++++');
+            containerPromo.classList.add('hide');
+            restaurants.classList.add('hide');
+            menu.classList.remove('hide');
+            cardsMenu.textContent = '';
+            createCardGood();
+            createCardGood();
+            createCardGood();
+            createCardGood();
+            toggleModalAuth();
+        }
     }
-  }
 }
 cardsRestaurants.addEventListener('click', openGoods);
 
 function home() {
-  containerPromo.classList.remove('hide');
-  restaurants.classList.remove('hide');
-  menu.classList.add('hide');
+    containerPromo.classList.remove('hide');
+    restaurants.classList.remove('hide');
+    menu.classList.add('hide');
 }
 logo.addEventListener('click', home);
 
@@ -181,6 +197,15 @@ createCardsRestaurant();
 
 //slider
 
-new Swiper ('.swiper-container', {
+new Swiper('.swiper-container', {
+    sliderPerView: 1,
+    loop: true,
+    autoplay: true,
+    effect: 'coverflow',
+    grabCursor: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    }
 });
 console.log(Swiper);
